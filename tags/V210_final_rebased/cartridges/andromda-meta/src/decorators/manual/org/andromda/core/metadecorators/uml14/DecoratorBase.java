2c3a8ddb44f53dcebc6b8175bfec7dfa8540d7fc
nobody <nobody@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2003-11-23 22:30:57 +0000

    This commit was manufactured by cvs2svn to create tag 'V210_final_rebased'.

---
+++ tags/V210_final_rebased/cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/DecoratorBase.java
@@ -0,0 +1,56 @@
+package org.andromda.core.metadecorators.uml14;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.omg.uml.foundation.core.ModelElement;
+
+/**
+ * Base class for all metaclass decorators.
+ */
+public class DecoratorBase
+{
+    private Object metaObject;
+
+    public DecoratorBase(Object metaObject)
+    {
+        this.metaObject = metaObject;
+    }
+
+    /**
+     * Returns a collection of decorators for a collection
+     * of metaobjects. Contacts the DecoratorFactory to manufacture
+     * the proper decorators.
+     * @see DecoratorFactory
+     * 
+     * @param metaobjects the objects to decorate
+     * @return Collection of DecoratorBase-derived objects
+     */
+    public static Collection decoratedElements(Collection metaobjects)
+    {
+        ArrayList result = new ArrayList(metaobjects.size());
+        DecoratorFactory df = DecoratorFactory.getInstance();
+
+        for (Iterator iter = metaobjects.iterator(); iter.hasNext();)
+        {
+            ModelElement element = (ModelElement) iter.next();
+            result.add(df.createDecoratorObject(element));
+        }
+        return result;
+    }
+
+    /**
+     * Returns one decorator for a particular metaobject. Contacts 
+     * the DecoratorFactory to manufacture the proper decorator.
+     * 
+     * @see DecoratorFactory
+     * @param metaObject the object to decorate
+     * @return DecoratorBase the decorator
+     */
+    public static DecoratorBase decoratedElement(ModelElement metaObject)
+    {
+        return DecoratorFactory.getInstance().createDecoratorObject(
+            metaObject);
+    }
+}
