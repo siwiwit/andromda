6318b9b257f6fe4724eb47c4526058df95be694d
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-17 21:25:36 +0000

    implemented validation

--- cartridges/andromda-bpm4struts/src/decorators/manual/org/andromda/cartridges/bpm4struts/metadecorators/uml14/StrutsActionStateDecoratorImpl.java
+++ cartridges/andromda-bpm4struts/src/decorators/manual/org/andromda/cartridges/bpm4struts/metadecorators/uml14/StrutsActionStateDecoratorImpl.java
@@ -1,7 +1,13 @@
 package org.andromda.cartridges.bpm4struts.metadecorators.uml14;
 
 import org.andromda.cartridges.bpm4struts.metadecorators.MetaDecoratorUtil;
+import org.andromda.core.metadecorators.uml14.DecoratorValidationException;
 import org.omg.uml.behavioralelements.statemachines.Transition;
+import org.omg.uml.behavioralelements.statemachines.StateMachine;
+import org.omg.uml.behavioralelements.activitygraphs.ActionState;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 
 /**
@@ -25,7 +31,7 @@ public class StrutsActionStateDecoratorImpl extends StrutsActionStateDecorator
     // abstract in class StrutsActionStateDecorator ...
     public String getDispatchMethodName()
     {
-        return MetaDecoratorUtil.toJavaMethodName(metaObject);
+        return MetaDecoratorUtil.toJavaMethodName(metaObject.getName());
     }
 
     public Integer getTriggerTransitionCount()
@@ -41,4 +47,36 @@ public class StrutsActionStateDecoratorImpl extends StrutsActionStateDecorator
 
     // ------------- relations ------------------
 
+    public void validate() throws DecoratorValidationException
+    {
+        // this state may not be owned by a composite state
+        if (metaObject.getContainer() != null)
+            throw new DecoratorValidationException(metaObject, "This state must be owned by a state machine, not a composite state");
+
+        // the name must not be empty
+        final String name = metaObject.getName();
+        if ( (name==null) || (name.trim().length()==0) )
+            throw new DecoratorValidationException(metaObject, "Name may not be empty or only contain whitespace");
+
+        // the name of the action state must be unique in the use-case state machine
+        final StateMachine stateMachine = metaObject.getStateMachine();
+        Collection actionStates = MetaDecoratorUtil.getSubvertices(stateMachine, MetaDecoratorUtil.FILTER_ACTIONSTATE);
+        actionStates.remove(metaObject);
+        // check that we don't accidentally generate the same names from more action states
+        final String dispatchMethodName = getDispatchMethodName();
+        for (Iterator iterator = actionStates.iterator(); iterator.hasNext();)
+        {
+            ActionState actionState = (ActionState) iterator.next();
+            if (dispatchMethodName.equals(MetaDecoratorUtil.toJavaMethodName(actionState.getName())))
+                throw new DecoratorValidationException(metaObject, "There is another action state in the same state machine which generates a name clash with this one, please change one of the action state\'s names");
+        }
+
+        // there must be at least one incoming transition
+        if (metaObject.getIncoming().isEmpty())
+            throw new DecoratorValidationException(metaObject, "There must be at least one transition going into this action state");
+
+        // there must be at least one outgoing transition
+        if (metaObject.getOutgoing().isEmpty())
+            throw new DecoratorValidationException(metaObject, "There must be at least one transition going out of this action state");
+    }
 }
