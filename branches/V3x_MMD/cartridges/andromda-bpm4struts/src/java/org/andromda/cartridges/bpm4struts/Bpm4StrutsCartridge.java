8279a570c7f7114f11bd002e3ceab5b57f24ee17
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-18 20:32:02 +0000

    registered the current set of decorators

--- branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsCartridge.java
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsCartridge.java
@@ -1,5 +1,19 @@
 package org.andromda.cartridges.bpm4struts;
 
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsActionStateDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsControllerDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsEventDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsFinalStateDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsGuardDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsInputFieldDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsModelDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsObjectFlowStateDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsPseudostateDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsSimpleStateDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsStateMachineDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsTransitionDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsUseCaseDecoratorImpl;
+import org.andromda.cartridges.bpm4struts.metadecorators.uml14.StrutsViewDecoratorImpl;
 import org.andromda.cartridges.interfaces.DefaultAndroMDACartridge;
 import org.andromda.core.metadecorators.uml14.DecoratorFactory;
 
@@ -13,28 +27,77 @@ public class Bpm4StrutsCartridge extends DefaultAndroMDACartridge {
         DecoratorFactory df = DecoratorFactory.getInstance();
         String oldNamespace = df.getActiveNamespace();
         df.setActiveNamespace (getDescriptor().getCartridgeName());
-/*
-        df.registerDecoratorClass(
-            "org.omg.uml.foundation.core.Operation$Impl",
-            EJBProfile.STEREOTYPE_FINDER_METHOD,
-            EJBFinderMethodDecoratorImpl.class.getName());
 
         df.registerDecoratorClass(
-            "org.omg.uml.foundation.core.Attribute$Impl",
-            EJBProfile.STEREOTYPE_PRIMARY_KEY,
-            EJBPrimaryKeyDecoratorImpl.class.getName());
-
-        df.registerDecoratorClass(
-            "org.omg.uml.foundation.core.Classifier$Impl",
-            EJBProfile.STEREOTYPE_ENTITY,
-            EJBEntityDecoratorImpl.class.getName());
-
-        df.registerDecoratorClass(
-            "org.omg.uml.foundation.core.AssociationEnd$Impl",
+            "org.omg.uml.behavioralelements.activitygraphs.ActionState$Impl",
             null,
-            EJBAssociationEndDecoratorImpl.class.getName());
+            StrutsActionStateDecoratorImpl.class.getName());
 
-*/
+        df.registerDecoratorClass(
+            "org.omg.uml.core.foundation.Classifier$Impl",
+            Bpm4StrutsProfile.STEREOTYPE_CONTROLLER,
+            StrutsControllerDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.statemachines.Event$Impl",
+            null,
+            StrutsEventDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.activitygraphs.FinalState$Impl",
+            null,
+            StrutsFinalStateDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.statemachines.Guard$Impl",
+            null,
+            StrutsGuardDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.core.foundation.Classifier$Impl",
+            Bpm4StrutsProfile.STEREOTYPE_INPUTFIELD,
+            StrutsInputFieldDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.core.foundation.Classifier$Impl",
+            Bpm4StrutsProfile.STEREOTYPE_MODEL,
+            StrutsModelDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.activitygraphs.ObjectFlowState$Impl",
+            null,
+            StrutsObjectFlowStateDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.statemachines.Pseudostate$Impl",
+            null,
+            StrutsPseudostateDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.statemachines.State$Impl",
+            null,
+            StrutsSimpleStateDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.statemachines.StateMachine$Impl",
+            null,
+            StrutsStateMachineDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.activitygraphs.Transition$Impl",
+            null,
+            StrutsTransitionDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.behavioralelements.usecases.UseCase$Impl",
+            null,
+            StrutsUseCaseDecoratorImpl.class.getName());
+
+        df.registerDecoratorClass(
+            "org.omg.uml.core.foundation.Classifier$Impl",
+            Bpm4StrutsProfile.STEREOTYPE_VIEW,
+            StrutsViewDecoratorImpl.class.getName());
+
         df.setActiveNamespace (oldNamespace);
     }
 }
