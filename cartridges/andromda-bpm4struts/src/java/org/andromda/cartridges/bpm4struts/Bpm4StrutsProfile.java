930ae6858d1cdc8796a6ef833485a0cd1b0c0315
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-18 20:32:29 +0000

    the current UML profile for bpm4struts, more tagged values will be added later

--- cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -16,7 +16,16 @@ public class Bpm4StrutsProfile
     public static final String TAGGED_VALUE_FORM_BEAN_NAME = "@org.andromda.struts.form.name";
     public static final String TAGGED_VALUE_ACTION_PATH = "@org.andromda.struts.action.path";
     public static final String TAGGED_VALUE_ACTION_STATE = "@org.andromda.struts.action.state";
+    public static final String TAGGED_VALUE_ACTION_PARAMETER = "@org.andromda.struts.action.parameter";
     public static final String TAGGED_VALUE_JSP_FILENAME = "@org.andromda.struts.jsp.filename";
+    public static final String TAGGED_VALUE_USE_CASE = "@org.andromda.struts.usecase";
+
+    /* ----------------- Default Values ------------------- */
+    public static final String TAGGED_VALUE_ACTION_DEFAULT_PARAMETER = "state";
+    public static final String DEFAULT_ABSTRACT_CLASS_SUFFIX = "Abstract";
+    public static final String DEFAULT_IMPLEMENTATION_CLASS_SUFFIX = "";
+    public static final String DEFAULT_ABSTRACT_METHOD_SUFFIX = "";
+    public static final String DEFAULT_IMPLEMENTATION_METHOD_SUFFIX = "Impl";
 
     private Bpm4StrutsProfile()
     {
