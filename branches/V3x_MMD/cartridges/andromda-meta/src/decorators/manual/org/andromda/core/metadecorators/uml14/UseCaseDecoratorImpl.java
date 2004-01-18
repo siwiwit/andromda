be03e27fc7c49d7a67ce9809ef20124aeaa72f19
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-18 00:37:33 +0000

    first implementation

---
+++ branches/V3x_MMD/cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/UseCaseDecoratorImpl.java
@@ -0,0 +1,79 @@
+package org.andromda.core.metadecorators.uml14;
+
+import org.andromda.core.common.CollectionFilter;
+import org.omg.uml.behavioralelements.activitygraphs.ActionState;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
+
+/**
+ *
+ * Metaclass decorator implementation for org.omg.uml.behavioralelements.usecases.UseCase
+ *
+ *
+ */
+public class UseCaseDecoratorImpl extends UseCaseDecorator
+{
+    // ---------------- constructor -------------------------------
+
+    public UseCaseDecoratorImpl(org.omg.uml.behavioralelements.usecases.UseCase metaObject)
+    {
+        super(metaObject);
+    }
+
+    // -------------------- business methods ----------------------
+
+    // concrete business methods that were declared
+    // abstract in class UseCaseDecorator ...
+
+    public java.util.Collection getStateMachines()
+    {
+        final CollectionFilter filter = new CollectionFilter()
+        {
+            public boolean accept(Object object)
+            {
+                return object instanceof ActionState;
+            }
+        };
+
+        return getSubGraphs(filter);
+    }
+
+    public java.util.Collection getActivityGraphs()
+    {
+        final CollectionFilter filter = new CollectionFilter()
+        {
+            public boolean accept(Object object)
+            {
+                return object instanceof ActionState;
+            }
+        };
+        
+        return getSubGraphs(filter);
+    }
+
+    protected Collection getSubGraphs(CollectionFilter collectionFilter)
+    {
+        return filter(metaObject.getOwnedElement(), collectionFilter);
+    }
+
+    private Collection filter(Collection collection, CollectionFilter collectionFilter)
+    {
+        final Set filteredCollection = new LinkedHashSet();
+        for (Iterator iterator = collection.iterator(); iterator.hasNext();)
+        {
+            Object object = iterator.next();
+            if (collectionFilter.accept(object))
+            {
+                filteredCollection.add(object);
+            }
+        }
+        return filteredCollection;
+    }
+
+    // ------------- relations ------------------
+
+}
