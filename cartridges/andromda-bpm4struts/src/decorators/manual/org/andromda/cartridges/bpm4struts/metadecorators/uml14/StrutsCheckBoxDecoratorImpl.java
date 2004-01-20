58e4b8e41abe782f1dc8a302f2de509712b0c9e5
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-20 18:47:10 +0000

    initial implementation

---
+++ cartridges/andromda-bpm4struts/src/decorators/manual/org/andromda/cartridges/bpm4struts/metadecorators/uml14/StrutsCheckBoxDecoratorImpl.java
@@ -0,0 +1,27 @@
+package org.andromda.cartridges.bpm4struts.metadecorators.uml14;
+
+
+
+/**
+ *
+ * Metaclass decorator implementation for org.omg.uml.foundation.core.Attribute
+ *
+ *
+ */
+public class StrutsCheckBoxDecoratorImpl extends StrutsCheckBoxDecorator
+{
+    // ---------------- constructor -------------------------------
+    
+    public StrutsCheckBoxDecoratorImpl (org.omg.uml.foundation.core.Attribute metaObject)
+    {
+        super (metaObject);
+    }
+
+    // -------------------- business methods ----------------------
+
+    // concrete business methods that were declared
+    // abstract in class StrutsCheckBoxDecorator ...
+
+    // ------------- relations ------------------
+    
+}
