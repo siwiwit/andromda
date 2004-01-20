7650c2f35f5a62c7e682f58a288af503435142df
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-20 18:49:01 +0000

    updated tagged value String constants: removed 'org.' added new stereotypes for the input fields added new tagged values for the input field properties

--- branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -7,22 +7,31 @@ public class Bpm4StrutsProfile
     public static final String STEREOTYPE_MODEL = "FrontEndModel";
     public static final String STEREOTYPE_CONTROLLER = "FrontEndController";
     public static final String STEREOTYPE_VIEW = "FrontEndView";
-    public static final String STEREOTYPE_INPUTFIELD = "FrontEndViewInputField";
+    public static final String STEREOTYPE_VIEW_TEXTFIELD = "FrontEndViewTextField";
+    public static final String STEREOTYPE_VIEW_TEXTAREA = "FrontEndViewTextArea";
+    public static final String STEREOTYPE_VIEW_CHECKBOX = "FrontEndViewTextCheckBox";
+    public static final String STEREOTYPE_VIEW_RADIOBUTTON = "FrontEndViewRadioButton";
+    public static final String STEREOTYPE_VIEW_COMBOBOX = "FrontEndViewComboBox";
+    public static final String STEREOTYPE_VIEW_LIST = "FrontEndViewList";
+    public static final String STEREOTYPE_VIEW_MULTIBOX = "FrontEndViewMultiBox";
+    public static final String STEREOTYPE_VIEW_PASSWORD = "FrontEndViewPassword";
     public static final String STEREOTYPE_WORKFLOW = "FrontEndWorkflow";
     public static final String STEREOTYPE_USECASE = "FrontEndUseCase";
 
     /* ----------------- Tagged Values -------------------- */
 
-    public static final String TAGGED_VALUE_FORM_BEAN_NAME = "@org.andromda.struts.form.name";
-    public static final String TAGGED_VALUE_ACTION_PATH = "@org.andromda.struts.action.path";
-    public static final String TAGGED_VALUE_ACTION_STATE = "@org.andromda.struts.action.state";
-    public static final String TAGGED_VALUE_ACTION_PARAMETER = "@org.andromda.struts.action.parameter";
-    public static final String TAGGED_VALUE_JSP_FILENAME = "@org.andromda.struts.jsp.filename";
-    public static final String TAGGED_VALUE_USE_CASE = "@org.andromda.struts.usecase";
-    public static final String TAGGED_VALUE_VIEW_ACTION = "@org.andromda.struts.view.action";
-    public static final String TAGGED_VALUE_INPUT_REQUIRED = "@org.andromda.struts.view.field.required";
-    public static final String TAGGED_VALUE_INPUT_READONLY = "@org.andromda.struts.view.field.readonly";
-    public static final String TAGGED_VALUE_INPUT_MAXLENGTH = "@org.andromda.struts.view.field.maxlength";
+    public static final String TAGGED_VALUE_FORM_BEAN_NAME = "@andromda.struts.form.name";
+    public static final String TAGGED_VALUE_ACTION_PATH = "@andromda.struts.action.path";
+    public static final String TAGGED_VALUE_ACTION_STATE = "@andromda.struts.action.state";
+    public static final String TAGGED_VALUE_ACTION_PARAMETER = "@andromda.struts.action.parameter";
+    public static final String TAGGED_VALUE_JSP_FILENAME = "@andromda.struts.jsp.filename";
+    public static final String TAGGED_VALUE_USE_CASE = "@andromda.struts.usecase";
+    public static final String TAGGED_VALUE_VIEW_ACTION = "@andromda.struts.view.action";
+    public static final String TAGGED_VALUE_INPUT_REQUIRED = "@andromda.struts.view.field.required";
+    public static final String TAGGED_VALUE_INPUT_SIZE = "@andromda.struts.view.field.size";
+    public static final String TAGGED_VALUE_INPUT_ROWS = "@andromda.struts.view.field.rows";
+    public static final String TAGGED_VALUE_INPUT_COLS = "@andromda.struts.view.field.cols";
+    public static final String TAGGED_VALUE_INPUT_PATTERN = "@andromda.struts.view.field.pattern";
 
     /* ----------------- Default Values ------------------- */
     public static final String TAGGED_VALUE_ACTION_DEFAULT_PARAMETER = "state";
