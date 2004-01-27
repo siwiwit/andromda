8fff05fa1f805b497574edb29a4f96e615f22f32
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-01-27 04:29:22 +0000

    move a few methods down to ClassifierDecorator since they are common

--- cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/ModelElementDecoratorImpl.java
+++ cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/ModelElementDecoratorImpl.java
@@ -3,10 +3,14 @@ package org.andromda.core.metadecorators.uml14;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.andromda.core.common.HTMLAnalyzer;
+import org.andromda.core.common.HTMLParagraph;
+import org.andromda.core.uml14.UMLProfile;
 import org.apache.commons.lang.StringUtils;
+import org.omg.uml.foundation.core.Comment;
 import org.omg.uml.foundation.core.ModelElement;
-import org.omg.uml.foundation.core.TaggedValue;
 import org.omg.uml.foundation.core.Stereotype;
+import org.omg.uml.foundation.core.TaggedValue;
 import org.omg.uml.foundation.datatypes.VisibilityKind;
 import org.omg.uml.foundation.datatypes.VisibilityKindEnum;
 import org.omg.uml.modelmanagement.Model;
@@ -19,6 +23,7 @@ import org.omg.uml.modelmanagement.Model;
  */
 public class ModelElementDecoratorImpl extends ModelElementDecorator
 {
+	
     // ---------------- constructor -------------------------------
 
     public ModelElementDecoratorImpl(
@@ -164,6 +169,65 @@ public class ModelElementDecoratorImpl extends ModelElementDecorator
     	}
 
     	return JavaVisibilityEnum.PACKAGE;
+    }
+    
+    /* (non-Javadoc)
+     * @see org.andromda.core.metadecorators.uml14.ModelElementDecorator#getDocumentation(java.lang.String)
+     */
+    public java.lang.String getDocumentation(String indent) {
+    	if (StringUtils.isEmpty(indent)) {
+    		indent = " ";
+    	}
+    	StringBuffer documentation = new StringBuffer();
+    	Collection comments = this.metaObject.getComment();
+    	if (comments != null && !comments.isEmpty()) {
+    		Iterator commentIt = comments.iterator();
+    		while (commentIt.hasNext()) {
+    			Comment comment = (Comment)commentIt.next();
+    			String commentString = StringUtils.trimToEmpty(comment.getBody());
+    			//if there isn't anything in the body, try the name
+    			if (StringUtils.isEmpty(commentString)) {
+    				commentString = StringUtils.trimToEmpty(comment.getName());
+    			}
+    			// if there still isn't anything, try a tagged value 
+    			if (StringUtils.isEmpty(commentString)) {
+    				commentString = StringUtils.trimToEmpty(
+    					this.findTaggedValue(UMLProfile.TAGGEDVALUE_DOCUMENTATION));
+    			}
+    			documentation.append(
+    					StringUtils.trimToEmpty(commentString));
+    		}
+    	}
+    	try {
+    		String star = "* ";
+    		String newLine = "\n";
+    		String startParaTag = "<p>";
+    		String endParaTag = "</p>";
+    		Collection paragraphs =
+    			new HTMLAnalyzer().htmlToParagraphs(documentation.toString());
+    		if (paragraphs != null && !paragraphs.isEmpty()) {
+    			documentation = new StringBuffer();
+    			Iterator paragraphIt = paragraphs.iterator();
+    			for (int ctr = 0; paragraphIt.hasNext(); ctr++) {
+    				HTMLParagraph paragraph = (HTMLParagraph)paragraphIt.next();
+    				documentation.append(indent + star + startParaTag + newLine);
+    				Collection lines = paragraph.getLines();
+    				if (lines != null && !lines.isEmpty()) {
+    					Iterator lineIt = lines.iterator();
+    					while (lineIt.hasNext()) {
+    						documentation.append(indent + star + lineIt.next() + newLine);
+    					}
+    				}
+    				documentation.append(indent + star + endParaTag);
+    			}
+    		} else {
+    			documentation.append(indent + star);
+    		}
+    	} catch (Throwable th) {
+    		String errMsg = "Error performing getDocumentationParagraphs";
+    		logger.error(errMsg, th);
+    	}
+    	return documentation.toString();
     }
 
 }
