0f01f558adb23c6ffde8cc6b8bb7e368a378c9bc
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-02-14 03:36:58 +0000

    remove no longer needed test code

--- cartridges/andromda-meta/src/test/org/andromda/core/metadecorators/uml14/DecoratorFactoryTest.java
+++ cartridges/andromda-meta/src/test/org/andromda/core/metadecorators/uml14/DecoratorFactoryTest.java
@@ -2,9 +2,6 @@ package org.andromda.core.metadecorators.uml14;
 
 import junit.framework.TestCase;
 
-import org.omg.uml.foundation.core.Attribute;
-import org.omg.uml.foundation.core.Classifier;
-
 public class DecoratorFactoryTest extends TestCase
 {
     /**
@@ -16,80 +13,22 @@ public class DecoratorFactoryTest extends TestCase
         super(arg0);
     }
 
-    public void testRegisterDecoratorClass()
+    public void testActiveNamespace()
     {
         DecoratorFactory df = DecoratorFactory.getInstance();
-        assertEquals(1, df.getNamespaceCount());
         
         df.setActiveNamespace("core");
         assertEquals("core", df.getActiveNamespace());
-        assertEquals(1, df.getNamespaceCount());
-        
-        int baseDecoratorCount = df.getDecoratorCount();
-
-        df.registerDecoratorClass(Classifier.class.getName(), null, SampleClassifierDecorator.class.getName());
-        assertEquals(baseDecoratorCount+1, df.getDecoratorCount());
-        df.registerDecoratorClass(Attribute.class.getName(), null, SampleAttributeDecorator.class.getName());
-        assertEquals(baseDecoratorCount+2, df.getDecoratorCount());
-        
-        String decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), null);
-        assertEquals(SampleClassifierDecorator.class.getName(), decoratorClassName);
-        
-        // even if stereotype is given, the normal decorator class should be returned
-        decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), "Entity");
-        assertEquals(SampleClassifierDecorator.class.getName(), decoratorClassName);
-        
-        decoratorClassName = df.lookupDecoratorClass(Attribute.class.getName(), null);
-        assertEquals(SampleAttributeDecorator.class.getName(), decoratorClassName);
-        
-        // even if stereotype is given, the normal decorator class should be returned
-        decoratorClassName = df.lookupDecoratorClass(Attribute.class.getName(), "PrimaryKey");
-        assertEquals(SampleAttributeDecorator.class.getName(), decoratorClassName);
-
-        // test negative case
-        decoratorClassName = df.lookupDecoratorClass("unregistered.class.Name", "PrimaryKey");
-        assertNull(decoratorClassName);
-        
-
-
+       
         df.setActiveNamespace("hibernate");
-        assertEquals(2, df.getNamespaceCount());
         assertEquals("hibernate", df.getActiveNamespace());
         
-        df.registerDecoratorClass(Classifier.class.getName(), "Entity", SampleEntityDecorator.class.getName());
-        assertEquals(1, df.getDecoratorCount());
-
-        // if stereotype is not given, the core's classifier decorator should be returned
-        decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), null);
-        assertEquals(SampleClassifierDecorator.class.getName(), decoratorClassName);
-        
-        // now, if stereotype is given, the decorator for entity classes should be returned
-        decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), "Entity");
-        assertEquals(SampleEntityDecorator.class.getName(), decoratorClassName);
-
-
-
-        df.setActiveNamespace("ejb");
-        assertEquals(3, df.getNamespaceCount());
-        assertEquals("ejb", df.getActiveNamespace());
-        
-        df.registerDecoratorClass(Classifier.class.getName(), "Entity", SampleEntityBeanDecorator.class.getName());
-        assertEquals(1, df.getDecoratorCount());
-
-        // now, if stereotype is given, the decorator for entity beans should be returned
-        decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), "Entity");
-        assertEquals(SampleEntityBeanDecorator.class.getName(), decoratorClassName);
-
-
-
-        df.setActiveNamespace("hibernate");
-        assertEquals(3, df.getNamespaceCount());
-        assertEquals("hibernate", df.getActiveNamespace());
-        
-        // now, if stereotype is given, the decorator for hibernate's entity classes should be returned again
-        // without interfering with the other registration for the stereotype "Entity"
-        decoratorClassName = df.lookupDecoratorClass(Classifier.class.getName(), "Entity");
-        assertEquals(SampleEntityDecorator.class.getName(), decoratorClassName);
+    }
+    
+    public void testRegisterProperty() {
+    	DecoratorFactory df = DecoratorFactory.getInstance();
+    	df.registerProperty("core", "testProperty", "SomeValue");   	
+    	assertEquals(df.getRegisteredProperty("core", "testProperty"), "SomeValue");    	
     }
 
 }
