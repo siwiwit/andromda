10ced7f8629416f0ab2b66cc250e641619799447
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-02-15 11:29:51 +0000

    start

---
+++ branches/V3x_MMD/cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/StateDecoratorImpl.java
@@ -0,0 +1,27 @@
+package org.andromda.core.metadecorators.uml14;
+
+
+
+/**
+ *
+ * Metaclass decorator implementation for org.omg.uml.behavioralelements.statemachines.State
+ *
+ *
+ */
+public class StateDecoratorImpl extends StateDecorator
+{
+    // ---------------- constructor -------------------------------
+    
+    public StateDecoratorImpl (org.omg.uml.behavioralelements.statemachines.State metaObject)
+    {
+        super (metaObject);
+    }
+
+    // -------------------- business methods ----------------------
+
+    // concrete business methods that were declared
+    // abstract in class StateDecorator ...
+
+    // ------------- relations ------------------
+    
+}
