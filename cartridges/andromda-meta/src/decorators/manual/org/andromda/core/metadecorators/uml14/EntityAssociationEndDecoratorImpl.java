577ee316fd7ec94f1823d1507d1ccaae79c2c558
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-02-15 04:22:44 +0000

    start moving toward adding JDBC and SQL mappings to the entity facades

---
+++ cartridges/andromda-meta/src/decorators/manual/org/andromda/core/metadecorators/uml14/EntityAssociationEndDecoratorImpl.java
@@ -0,0 +1,34 @@
+package org.andromda.core.metadecorators.uml14;
+
+
+
+/**
+ *
+ * Metaclass decorator implementation for org.omg.uml.foundation.core.AssociationEnd
+ *
+ *
+ */
+public class EntityAssociationEndDecoratorImpl extends EntityAssociationEndDecorator
+{
+    // ---------------- constructor -------------------------------
+    
+    public EntityAssociationEndDecoratorImpl (org.omg.uml.foundation.core.AssociationEnd metaObject)
+    {
+        super (metaObject);
+    }
+
+    // -------------------- business methods ----------------------
+
+    // concrete business methods that were declared
+    // abstract in class EntityAssociationEndDecorator ...
+
+    public java.lang.String getColumnName() {
+        // TODO: put your implementation here.
+
+        // Dummy return value, just that the file compiles
+        return null;
+    }
+
+    // ------------- relations ------------------
+    
+}
