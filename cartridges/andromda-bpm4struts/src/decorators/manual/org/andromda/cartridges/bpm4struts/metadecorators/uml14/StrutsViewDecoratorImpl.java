858f2d8aa11496b5815daa9ab5a3cbdfda6cac47
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-02-22 14:57:51 +0000

    added 'getFirstInputField', to determine which widget to give initial focus improved message keys

--- cartridges/andromda-bpm4struts/src/decorators/manual/org/andromda/cartridges/bpm4struts/metadecorators/uml14/StrutsViewDecoratorImpl.java
+++ cartridges/andromda-bpm4struts/src/decorators/manual/org/andromda/cartridges/bpm4struts/metadecorators/uml14/StrutsViewDecoratorImpl.java
@@ -106,6 +106,7 @@ public class StrutsViewDecoratorImpl extends StrutsViewDecorator
 
     public Map getTriggerMessages()
     {
+/*
         Map triggerMessages = new TreeMap();
         String keyPrefix = StringUtilsHelper.separate(getName(), ".") + '.';
         Collection triggers = getTriggers();
@@ -117,11 +118,18 @@ public class StrutsViewDecoratorImpl extends StrutsViewDecorator
             triggerMessages.put(key, val);
         }
         return triggerMessages;
+*/
+        return null;    // todo remove this method
+    }
+
+    public String getMessageKey()
+    {
+        return StringUtilsHelper.separate(getName(), ".").toLowerCase();
     }
 
     public String getTitleMessageKey()
     {
-        return (StringUtilsHelper.separate(getName(), ".") + ".title").toLowerCase();
+        return getMessageKey() + ".title";
     }
 
     public String getTitleMessageValue()
@@ -147,6 +155,12 @@ public class StrutsViewDecoratorImpl extends StrutsViewDecorator
     public String getValidationMethodName()
     {
         return "validate" + StringUtilsHelper.upperCaseFirstLetter(getFormBeanName());
+    }
+
+    public StrutsInputFieldDecorator getFirstInputField()
+    {
+        Collection inputFields = getInputFields();
+        return (inputFields.isEmpty()) ? null : (StrutsInputFieldDecorator)inputFields.iterator().next();
     }
 
     // ------------- relations ------------------
