e748c7f67e380fd0d9670127ecd6a911b79d4164
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-05 21:05:54 +0000

    first commit for the new design

--- branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -4,11 +4,12 @@ public class Bpm4StrutsProfile
 {
     /* ----------------- Stereotypes -------------------- */
 
-    public static final String STEREOTYPE_MODEL = "FrontEndModel";
+    public static final String STEREOTYPE_VIEW = "FrontEndView";
+    public static final String STEREOTYPE_EVENT = "FrontEndEvent";
+    public static final String STEREOTYPE_ACTOR = "FrontEndActor";
     public static final String STEREOTYPE_CONTROLLER = "FrontEndController";
     public static final String STEREOTYPE_EXCEPTION = "FrontEndException";
-    public static final String STEREOTYPE_MESSAGE = "FrontEndMessage";
-    public static final String STEREOTYPE_VIEW = "FrontEndView";
+
     public static final String STEREOTYPE_VIEW_TEXTFIELD = "FrontEndViewTextField";
     public static final String STEREOTYPE_VIEW_TEXTAREA = "FrontEndViewTextArea";
     public static final String STEREOTYPE_VIEW_CHECKBOX = "FrontEndViewTextCheckBox";
@@ -17,19 +18,11 @@ public class Bpm4StrutsProfile
     public static final String STEREOTYPE_VIEW_LIST = "FrontEndViewList";
     public static final String STEREOTYPE_VIEW_MULTIBOX = "FrontEndViewMultiBox";
     public static final String STEREOTYPE_VIEW_PASSWORD = "FrontEndViewPassword";
-    public static final String STEREOTYPE_WORKFLOW = "FrontEndWorkflow";
-    public static final String STEREOTYPE_USECASE = "FrontEndUseCase";
 
     /* ----------------- Tagged Values -------------------- */
 
-    public static final String TAGGED_VALUE_FORM_BEAN_NAME = "@andromda.struts.form.name";
-    public static final String TAGGED_VALUE_ACTION_PATH = "@andromda.struts.action.path";
-    public static final String TAGGED_VALUE_ACTION_STATE = "@andromda.struts.action.state";
-    public static final String TAGGED_VALUE_ACTION_TRIGGER = "@andromda.struts.action.trigger";
-    public static final String TAGGED_VALUE_ACTION_PARAMETER = "@andromda.struts.action.parameter";
-    public static final String TAGGED_VALUE_JSP_FILENAME = "@andromda.struts.jsp.filename";
-    public static final String TAGGED_VALUE_USE_CASE = "@andromda.struts.usecase";
-    public static final String TAGGED_VALUE_VIEW_ACTION = "@andromda.struts.view.action";
+    public static final String TAGGED_VALUE_ACTION_TYPE = "@andromda.struts.action.type";
+    public static final String TAGGED_VALUE_ACTION_SUCCES_MESSAGE = "@andromda.struts.action.succes.message";
     public static final String TAGGED_VALUE_INPUT_REQUIRED = "@andromda.struts.view.field.required";
     public static final String TAGGED_VALUE_INPUT_SIZE = "@andromda.struts.view.field.size";
     public static final String TAGGED_VALUE_INPUT_ROWS = "@andromda.struts.view.field.rows";
@@ -41,12 +34,11 @@ public class Bpm4StrutsProfile
     public static final String TAGGED_VALUE_EXCEPTION_KEY = "@andromda.struts.exception.key";
 
     /* ----------------- Default Values ------------------- */
-    public static final String TAGGED_VALUE_ACTION_DEFAULT_PARAMETER = "___state";
+    public static final String TAGGED_VALUE_INPUT_DEFAULT_REQUIRED = "true";
+
+    public static final String TAGGED_VALUE_ACTION_TYPE_HYPERLINK = "hyperlink";
+    public static final String TAGGED_VALUE_ACTION_TYPE_FORM = "form";
     public static final String TAGGED_VALUE_EXCEPTION_DEFAULT_TYPE = "java.lang.Exception";
-    public static final String DEFAULT_ABSTRACT_CLASS_SUFFIX = "Abstract";
-    public static final String DEFAULT_IMPLEMENTATION_CLASS_SUFFIX = "";
-    public static final String DEFAULT_ABSTRACT_METHOD_SUFFIX = "";
-    public static final String DEFAULT_IMPLEMENTATION_METHOD_SUFFIX = "Impl";
 
     private Bpm4StrutsProfile()
     {
