0015b3e42175f338837d9a7e0313ef616ef45dc1
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-07 22:20:50 +0000

    added getFullPathName, getActions and getDecisionChoices

--- branches/V3x_MMD/cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsControllerLogicImpl.java
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsControllerLogicImpl.java
@@ -1,5 +1,10 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 
 /**
  * MetafacadeLogic implementation.
@@ -46,6 +51,61 @@ public class StrutsControllerLogicImpl
         return getFullyQualifiedName();
     }
 
+    public java.lang.String getFullPathName()
+    {
+        return '/' + getFullyQualifiedName().replace('.','/');
+    }
+
+    public java.util.Collection getDecisionChoices()
+    {
+        Collection choices = new HashSet();
+
+        Collection actions = getActions();
+        for (Iterator iterator = actions.iterator(); iterator.hasNext();)
+        {
+            StrutsAction action = (StrutsAction) iterator.next();
+            if (action.isTargettingDecisionPoint())
+            {
+                Collection outcomes = action.getTarget().getOutgoing();
+                for (Iterator outcomeIterator = outcomes.iterator(); outcomeIterator.hasNext();)
+                {
+                    StrutsTransition transition = (StrutsTransition) outcomeIterator.next();
+                    choices.add(transition.getGuardName());
+                }
+            }
+        }
+        return choices;
+    }
+
     // ------------- relations ------------------
+
+    protected Collection handleGetActions()
+    {
+        final Collection actions = new LinkedList();
+
+        Collection useCases = getModel().getAllUseCases();
+        for (Iterator useCaseIterator = useCases.iterator(); useCaseIterator.hasNext();)
+        {
+            Object obj = useCaseIterator.next();
+            if (obj instanceof StrutsUseCase)
+            {
+                StrutsUseCase useCase = (StrutsUseCase)obj;
+                StrutsActivityGraph graph = useCase.getActivityGraph();
+                if (graph.getContextElement().equals(this))
+                {
+                    Collection actionStates = graph.getActionStates();
+                    for (Iterator actionStateIterator = actionStates.iterator(); actionStateIterator.hasNext();)
+                    {
+                        Object jsp = (Object) actionStateIterator.next();
+                        if (jsp instanceof StrutsJsp)
+                        {
+                            actions.addAll(((StrutsJsp)jsp).getOutgoing());
+                        }
+                    }
+                }
+            }
+        }
+        return actions;
+    }
 
 }
