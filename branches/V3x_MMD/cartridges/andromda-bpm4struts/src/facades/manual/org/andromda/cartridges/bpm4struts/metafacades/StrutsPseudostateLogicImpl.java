2f086e1edfbf97606fe5bb67bbc9b6306e28c86e
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-09 23:40:48 +0000

    new class as part of the new design

---
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsPseudostateLogicImpl.java
@@ -0,0 +1,37 @@
+package org.andromda.cartridges.bpm4struts.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation.
+ *
+ * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsPseudostate
+ */
+public class StrutsPseudostateLogicImpl
+       extends StrutsPseudostateLogic
+       implements org.andromda.cartridges.bpm4struts.metafacades.StrutsPseudostate
+{
+    // ---------------- constructor -------------------------------
+    
+    public StrutsPseudostateLogicImpl (java.lang.Object metaObject, java.lang.String context)
+    {
+        super (metaObject, context);
+    }
+
+    // -------------------- business methods ----------------------
+
+    // concrete business methods that were declared
+    // abstract in class StrutsPseudostate ...
+
+	/**
+	 * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsPseudostate#getActionMethodName()()
+	 */
+    public java.lang.String getActionMethodName() {
+        // TODO: put your implementation here.
+
+        // Dummy return value, just that the file compiles
+        return null;
+    }
+
+    // ------------- relations ------------------
+    
+}
