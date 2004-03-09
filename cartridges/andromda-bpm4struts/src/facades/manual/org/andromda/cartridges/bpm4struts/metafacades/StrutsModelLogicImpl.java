ca04aef687c3dbd44aab65205ce4424576df267b
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-09 00:22:26 +0000

    getPages() and getUsers() moved to the StrutsApplication facade

--- cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsModelLogicImpl.java
+++ cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsModelLogicImpl.java
@@ -1,10 +1,6 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
-import org.omg.uml.UmlPackage;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 
 /**
@@ -16,49 +12,11 @@ public class StrutsModelLogicImpl
         extends StrutsModelLogic
         implements org.andromda.cartridges.bpm4struts.metafacades.StrutsModel
 {
-    private UmlPackage model = null;
-
     // ---------------- constructor -------------------------------
     
     public StrutsModelLogicImpl(java.lang.Object metaObject, java.lang.String context)
     {
         super(metaObject, context);
-        this.model = (UmlPackage)metaObject;
-    }
-    // ------------- relations ------------------
-
-    /**
-     * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsModel#getUsers()
-     */
-    public java.util.Collection handleGetUsers()
-    {
-        final Collection users = new LinkedList();
-        final Collection allActors = model.getUseCases().getActor().refAllOfType();
-
-        for (Iterator actorIterator = allActors.iterator(); actorIterator.hasNext();)
-        {
-            Object actor = shieldedElement(actorIterator.next());
-            if (actor instanceof StrutsUser)
-                users.add(actor);
-        }
-        return users;
-    }
-
-    /**
-     * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsModel#getPages()
-     */
-    public java.util.Collection handleGetPages()
-    {
-        final Collection pages = new LinkedList();
-        final Collection allActionStates = model.getUseCases().getActor().refAllOfType();
-
-        for (Iterator actionStateIterator = allActionStates.iterator(); actionStateIterator.hasNext();)
-        {
-            Object actionState = shieldedElement(actionStateIterator.next());
-            if (actionState instanceof StrutsJsp)
-                pages.add(actionState);
-        }
-        return pages;
     }
 
 }
