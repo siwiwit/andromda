3c4f2a5d7754fc415940f83b371cd9c6de197be3
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-12 00:08:32 +0000

    added the front end use-case

--- branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ branches/V3x_MMD/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -7,6 +7,7 @@ public class Bpm4StrutsProfile
     public static final String STEREOTYPE_VIEW = "FrontEndView";
     public static final String STEREOTYPE_EVENT = "FrontEndEvent";
     public static final String STEREOTYPE_USER = "FrontEndUser";
+    public static final String STEREOTYPE_USECASE = "FrontEndUseCase";
     public static final String STEREOTYPE_CONTROLLER = "FrontEndController";
     public static final String STEREOTYPE_EXCEPTION = "FrontEndException";
 
