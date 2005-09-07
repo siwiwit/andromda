ea2704fb8ed2651c847208c4f973ee98b2040e0c
nobody <nobody@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-09-07 12:03:22 +0000

    This commit was manufactured by cvs2svn to create tag 'V3_1RC1_tag'.

---
+++ tags/V3_1RC1_tag/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/EventFacadeLogicImpl.java
@@ -0,0 +1,76 @@
+package org.andromda.metafacades.uml14;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.omg.uml.behavioralelements.statemachines.State;
+import org.omg.uml.behavioralelements.statemachines.Transition;
+
+
+/**
+ * MetafacadeLogic implementation.
+ *
+ * @see org.andromda.metafacades.uml.EventFacade
+ */
+public class EventFacadeLogicImpl
+    extends EventFacadeLogic
+{
+    public EventFacadeLogicImpl(
+        org.omg.uml.behavioralelements.statemachines.Event metaObject,
+        java.lang.String context)
+    {
+        super(metaObject, context);
+    }
+
+    protected Collection handleGetParameters()
+    {
+        return metaObject.getParameter();
+    }
+
+    protected Object handleGetTransition()
+    {
+        Transition eventTransition = null;
+
+        final Collection allTransitions =
+            UML14MetafacadeUtils.getModel().getStateMachines().getTransition().refAllOfType();
+        for (final Iterator iterator = allTransitions.iterator(); iterator.hasNext() && eventTransition == null;)
+        {
+            final Transition transition = (Transition)iterator.next();
+            if (metaObject.equals(transition.getTrigger()))
+            {
+                eventTransition = transition;
+            }
+        }
+
+        return eventTransition;
+    }
+
+    protected Object handleGetState()
+    {
+        State eventState = null;
+
+        final Collection allStates = UML14MetafacadeUtils.getModel().getStateMachines().getState().refAllOfType();
+        for (final Iterator stateIterator = allStates.iterator(); stateIterator.hasNext() && eventState == null;)
+        {
+            final State state = (State)stateIterator.next();
+            if (state.getDeferrableEvent().contains(metaObject))
+            {
+                eventState = state;
+            }
+        }
+
+        return eventState;
+    }
+
+    public Object getValidationOwner()
+    {
+        Object validationOwner = getTransition();
+
+        if (validationOwner == null)
+        {
+            validationOwner = getState();
+        }
+
+        return validationOwner;
+    }
+}
