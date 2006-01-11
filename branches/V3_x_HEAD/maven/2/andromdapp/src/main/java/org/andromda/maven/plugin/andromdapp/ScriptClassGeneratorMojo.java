e2a84f075a84ac2a8568267445f6e73033a9fc0c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-01-11 00:38:13 +0000

    add provided dependencies to classpath

--- branches/V3_x_HEAD/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/ScriptClassGeneratorMojo.java
+++ branches/V3_x_HEAD/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/ScriptClassGeneratorMojo.java
@@ -1,11 +1,10 @@
 package org.andromda.maven.plugin.andromdapp;
 
 import java.io.File;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -13,7 +12,11 @@ import java.util.Set;
 
 import org.andromda.core.common.ClassUtils;
 import org.andromda.maven.plugin.andromdapp.script.ScriptClassGenerator;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -27,7 +30,7 @@ import org.apache.maven.project.MavenProject;
  * @author Chad Brandon
  * @goal instrument-scripts
  * @phase compile
- * @requiresDependencyResolution runtime
+ * @requiresDependencyResolution
  */
 public class ScriptClassGeneratorMojo
     extends AbstractMojo
@@ -56,6 +59,20 @@ public class ScriptClassGeneratorMojo
     private MavenProject project;
 
     /**
+     * @parameter expression="${component.org.apache.maven.artifact.factory.ArtifactFactory}"
+     * @required
+     * @readonly
+     */
+    protected ArtifactFactory factory;
+
+    /**
+     * @parameter expression="${localRepository}"
+     * @required
+     * @readonly
+     */
+    protected ArtifactRepository localRepository;
+
+    /**
      * The java file extension
      */
     private static final String JAVA_EXTENSION = ".java";
@@ -71,7 +88,9 @@ public class ScriptClassGeneratorMojo
             final ScriptClassGenerator generator = ScriptClassGenerator.getInstance(this.scriptWrapper);
             if (this.locations != null)
             {
-                this.initializeClassLoader(this.project.getRuntimeClasspathElements());
+                final List classpathElements = new ArrayList(this.getProvidedClasspathElements());
+                classpathElements.addAll(this.project.getRuntimeClasspathElements());
+                this.initializeClassLoader(classpathElements);
                 for (int ctr = 0; ctr < locations.length; ctr++)
                 {
                     final Location location = locations[ctr];
@@ -100,6 +119,62 @@ public class ScriptClassGeneratorMojo
         {
             throw new MojoExecutionException("Failed to inject script wrappers", throwable);
         }
+    }
+
+    /**
+     * Adds any dependencies to the current project from the plugin
+     * having the given <code>pluginArtifactId</code>.
+     *
+     * @param pluginArtifactId the artifactId of the plugin of which to add its dependencies.
+     * @param scope the artifact scope in which to add them (runtime, compile, etc).
+     */
+    protected List getProvidedClasspathElements()
+    {
+        final List classpathElements = new ArrayList();
+        for (final Iterator iterator = this.project.getDependencies().iterator(); iterator.hasNext();)
+        {
+            final Artifact artifact = this.getArtifact(
+                (Dependency)iterator.next(),
+                Artifact.SCOPE_PROVIDED);
+            if (artifact != null)
+            {
+                classpathElements.add(artifact.getFile().getAbsolutePath());
+            }
+        }
+        return classpathElements;
+    }
+
+    /**
+     * Adds a dependency to the current project's dependencies.
+     *
+     * @param dependency
+     * @param scope the scope of the artifact
+     */
+    private Artifact getArtifact(
+        final Dependency dependency,
+        final String scope)
+    {
+        Artifact artifact = null;
+        final ArtifactRepository localRepository = this.localRepository;
+        final MavenProject project = this.project;
+        if (project != null && localRepository != null)
+        {
+            if (dependency != null)
+            {
+                artifact =
+                    this.factory.createArtifact(
+                        dependency.getGroupId(),
+                        dependency.getArtifactId(),
+                        dependency.getVersion(),
+                        scope,
+                        dependency.getType());
+                final File file = new File(
+                        localRepository.getBasedir(),
+                        localRepository.pathOf(artifact));
+                artifact.setFile(file);
+            }
+        }
+        return artifact;
     }
 
     /**
