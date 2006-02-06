4b7a324859ee85b2cee6cb4fcca6cb2c7f2f1070
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-02-06 14:27:44 +0000

    added javadoc

--- branches/V3_x_HEAD/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/hibernate/HibernateValidateSchema.java
+++ branches/V3_x_HEAD/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/hibernate/HibernateValidateSchema.java
@@ -8,22 +8,37 @@ import java.util.List;
 import java.util.Random;
 import java.io.File;
 
+/**
+ * Provides the ability to validate a schema from Hibernate
+ * mapping files.
+ *
+ * @author Wouter Zoons
+ */
 public class HibernateValidateSchema
     extends HibernateSchemaManagement
 {
+    private static final String HIBERNATE_PROPERTIES_TEMP_DIRECTORY =
+        Constants.TEMPORARY_DIRECTORY + "andromdapp/hibernate-schema-validate";
+
+    /**
+     * @see org.andromda.maven.plugin.andromdapp.hibernate.HibernateSchemaManagement#getExecutionClassName()
+     */
     protected String getExecutionClassName()
     {
         return "SchemaValidator";
     }
 
+    /**
+     * @see org.andromda.maven.plugin.andromdapp.hibernate.HibernateSchemaManagement#getExecutionOuputPath(java.util.Map)
+     */
     protected String getExecutionOuputPath(final Map options)
     {
         return null;
     }
 
-    private static final String HIBERNATE_PROPERTIES_TEMP_DIRECTORY =
-        Constants.TEMPORARY_DIRECTORY + "andromdapp/hibernate-schema-validate";
-
+    /**
+     * @see org.andromda.maven.plugin.andromdapp.hibernate.HibernateSchemaManagement#addArguments(java.util.Map, java.util.List)
+     */
     protected void addArguments(final Map options, final List arguments) throws Exception
     {
         final String driverClass = this.getRequiredProperty(
@@ -39,10 +54,10 @@ public class HibernateValidateSchema
                 options,
                 "jdbcPassword");
         final StringBuffer contents = new StringBuffer();
-        contents.append("hibernate.connection.driver_class=" + driverClass + "\n");
-        contents.append("hibernate.connection.url=" + connectionUrl + "\n");
-        contents.append("hibernate.connection.username=" + username + "\n");
-        contents.append("hibernate.connection.password=" + password + "\n");
+        contents.append("hibernate.connection.driver_class=").append(driverClass).append("\n");
+        contents.append("hibernate.connection.url=").append(connectionUrl).append("\n");
+        contents.append("hibernate.connection.username=").append(username).append("\n");
+        contents.append("hibernate.connection.password=").append(password).append("\n");
         final File temporaryProperitesFile =
             new File(HIBERNATE_PROPERTIES_TEMP_DIRECTORY, new Random().nextDouble() + "");
         temporaryProperitesFile.deleteOnExit();
