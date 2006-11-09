04193f238b971284f68603ea571a8e604ac36ea6
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-11-09 11:03:29 +0000

    Added method to determine whether a PSMmetaclass implementation class must be abstract or not to help the templates generate correct code.

--- branches/V3_x_HEAD/cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/PSMmetaclassLogicImpl.java
+++ branches/V3_x_HEAD/cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/PSMmetaclassLogicImpl.java
@@ -1,18 +1,21 @@
 package org.andromda.cartridges.meta.metafacades;
 
+import java.util.Iterator;
+
+import org.andromda.metafacades.uml.OperationFacade;
 
 /**
- * MetafacadeLogic implementation for org.andromda.cartridges.meta.metafacades.PSMmetaclass.
- *
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.meta.metafacades.PSMmetaclass.
+ * 
  * @see org.andromda.cartridges.meta.metafacades.PSMmetaclass
  */
-public class PSMmetaclassLogicImpl
-    extends PSMmetaclassLogic
+public class PSMmetaclassLogicImpl extends PSMmetaclassLogic
 {
 
-    public PSMmetaclassLogicImpl (Object metaObject, String context)
+    public PSMmetaclassLogicImpl(Object metaObject, String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
     }
 
     /**
@@ -21,6 +24,38 @@ public class PSMmetaclassLogicImpl
     protected boolean handleIsOperationsPresent()
     {
         return this.getOperations().size() > 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.andromda.cartridges.meta.metafacades.PSMmetaclassLogic#handleIsImplMustBeAbstract()
+     */
+    protected boolean handleIsImplMustBeAbstract()
+    {
+        boolean result = false;
+
+        // if the class itself is abstract, make the impl abstract, too.
+        if (this.isAbstract())
+        {
+            result = true;
+        }
+        else
+        {
+            // if the class contains abstract operations, the impl must be
+            // abstract, too, because the abstract operations will not be
+            // generated as methods.
+            for (Iterator iter = this.getOperations().iterator(); iter.hasNext();)
+            {
+                OperationFacade operation = (OperationFacade) iter.next();
+                if (operation.isAbstract())
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
 }
