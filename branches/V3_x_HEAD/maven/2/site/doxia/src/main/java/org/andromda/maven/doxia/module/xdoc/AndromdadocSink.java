25ccb0b09e0ab41440f0df1b0851181029aaa651
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2007-01-10 00:21:13 +0000

    updated headers

--- branches/V3_x_HEAD/maven/2/site/doxia/src/main/java/org/andromda/maven/doxia/module/xdoc/AndromdadocSink.java
+++ branches/V3_x_HEAD/maven/2/site/doxia/src/main/java/org/andromda/maven/doxia/module/xdoc/AndromdadocSink.java
@@ -1,19 +1,7 @@
 package org.andromda.maven.doxia.module.xdoc;
 
 /*
- * Copyright 2004-2006 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Based on The Apache Software Foundation XdocSink
  */
 
 import org.apache.maven.doxia.module.HtmlTools;
@@ -31,7 +19,7 @@ import java.io.Writer;
  *
  * Based taken from Apache Foundation Doxia Project.
  * 
- * @version $Id: AndromdadocSink.java,v 1.1.2.2 2007-01-09 03:20:41 vancek Exp $
+ * @version $Id: AndromdadocSink.java,v 1.1.2.3 2007-01-10 00:21:13 vancek Exp $
  * 
  * @plexus.component role="org.apache.maven.doxia.sink.Sink" role-hint="andromdadoc"
  * 
--- branches/V3_x_HEAD/maven/2/site/doxia/src/main/java/org/andromda/maven/doxia/module/xdoc/AndromdadocSiteModule.java
+++ branches/V3_x_HEAD/maven/2/site/doxia/src/main/java/org/andromda/maven/doxia/module/xdoc/AndromdadocSiteModule.java
@@ -1,25 +1,13 @@
 package org.andromda.maven.doxia.module.xdoc;
 
 /*
- * Copyright 2004-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Based on The Apache Software Foundation XdocModule
  */
 
 import org.apache.maven.doxia.site.module.SiteModule;
 
 /**
- * @version $Id: AndromdadocSiteModule.java,v 1.1.2.2 2007-01-09 03:20:41 vancek Exp $
+ * @version $Id: AndromdadocSiteModule.java,v 1.1.2.3 2007-01-10 00:21:13 vancek Exp $
  * 
  * Based taken from Apache Foundation Doxia Project.
  * 
