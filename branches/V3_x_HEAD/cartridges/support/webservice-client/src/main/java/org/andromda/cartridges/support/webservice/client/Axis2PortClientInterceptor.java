0ed638b9643c2b84ba3fcbb07a87a10887b8ee42
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2007-06-20 22:20:55 +0000

    typo

--- branches/V3_x_HEAD/cartridges/support/webservice-client/src/main/java/org/andromda/cartridges/support/webservice/client/Axis2PortClientInterceptor.java
+++ branches/V3_x_HEAD/cartridges/support/webservice-client/src/main/java/org/andromda/cartridges/support/webservice/client/Axis2PortClientInterceptor.java
@@ -229,7 +229,7 @@ public class Axis2PortClientInterceptor
     {
         if (AopUtils.isToStringMethod(invocation.getMethod()))
         {
-            return "Axis2 proxy for Port [" + this.getPortAddress() + "] of service [" +
+            return "Axis2 proxy for port [" + this.getPortAddress() + "] of service [" +
                 this.getServiceInterface().getName() + "]";
         }
         if (logger.isDebugEnabled())
