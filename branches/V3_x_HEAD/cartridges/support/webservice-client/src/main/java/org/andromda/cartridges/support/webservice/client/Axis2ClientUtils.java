7067e8d5f8bc4d1671ef8738ace9e13f2cdab876
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2007-07-18 19:53:22 +0000

    get rid of debug

--- branches/V3_x_HEAD/cartridges/support/webservice-client/src/main/java/org/andromda/cartridges/support/webservice/client/Axis2ClientUtils.java
+++ branches/V3_x_HEAD/cartridges/support/webservice-client/src/main/java/org/andromda/cartridges/support/webservice/client/Axis2ClientUtils.java
@@ -304,7 +304,6 @@ public class Axis2ClientUtils
                 }
                 catch (final Throwable throwable)
                 {
-                    throwable.printStackTrace();
                     throw new RuntimeException(throwable);
                 }
             }
