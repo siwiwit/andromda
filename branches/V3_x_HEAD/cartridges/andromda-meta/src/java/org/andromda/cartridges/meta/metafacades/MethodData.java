c7322ceb1c98ea3b16431ddad1a3dfe35ba1eb1c
cissou <cissou@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2009-05-07 23:01:15 +0000

    Fixed issue META-11. Insure that the MethodData instances are sorted in a deterministic way. Updated the expected cartridge output.

--- branches/V3_x_HEAD/cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/MethodData.java
+++ branches/V3_x_HEAD/cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/MethodData.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-
 /**
  * @author <a href="http://www.mbohlen.de">Matthias Bohlen </a>
  * @author Chad Brandon
+ * @author Cyril Combe
  * @since 10.12.2003
  */
-public class MethodData
-    implements Comparable
+public class MethodData implements Comparable
 {
     private String metafacadeName;
     private String visibility;
@@ -63,7 +62,7 @@ public class MethodData
 
     /**
      * Gets the metafacade name.
-     *
+     * 
      * @return the name of the metafacade
      */
     public String getMetafacadeName()
@@ -73,7 +72,7 @@ public class MethodData
 
     /**
      * Gets the name of the method.
-     *
+     * 
      * @return the name.
      */
     public String getName()
@@ -83,7 +82,7 @@ public class MethodData
 
     /**
      * Gets the name of the return type for this method.
-     *
+     * 
      * @return the return type name.
      */
     public String getReturnTypeName()
@@ -93,15 +92,16 @@ public class MethodData
 
     /**
      * Builds a string representing a declaration for this method.
-     *
-     * @param suppressAbstractDeclaration optionally suppress the "abstract" modifier
+     * 
+     * @param suppressAbstractDeclaration
+     *            optionally suppress the "abstract" modifier
      * @return String the declaration
      */
     public String buildMethodDeclaration(boolean suppressAbstractDeclaration)
     {
-        String declaration =
-            visibility + " " + ((isAbstract && !suppressAbstractDeclaration) ? "abstract " : "") +
-            ((returnTypeName != null) ? (returnTypeName + " ") : "") + name + "(";
+        String declaration = visibility + " "
+                + ((isAbstract && !suppressAbstractDeclaration) ? "abstract " : "")
+                + ((returnTypeName != null) ? (returnTypeName + " ") : "") + name + "(";
 
         for (final Iterator iterator = arguments.iterator(); iterator.hasNext();)
         {
@@ -133,7 +133,7 @@ public class MethodData
 
     /**
      * Builds a string representing a call to the method.
-     *
+     * 
      * @return String how a call would look like
      */
     public String buildMethodCall()
@@ -156,7 +156,7 @@ public class MethodData
     /**
      * Builds a signature which can be used as a key into a map. Consists of the return type, the name and the f.q.
      * types of the arguements.
-     *
+     * 
      * @return String the key that identifies this method
      */
     public String buildCharacteristicKey()
@@ -179,7 +179,7 @@ public class MethodData
 
     /**
      * Indicates whether or not this method is abstract.
-     *
+     * 
      * @return true/false
      */
     public boolean isAbstract()
@@ -189,7 +189,7 @@ public class MethodData
 
     /**
      * Gets the visibility of this method.
-     *
+     * 
      * @return the visibility.
      */
     public String getVisibility()
@@ -199,7 +199,7 @@ public class MethodData
 
     /**
      * Gets the documentation for this method.
-     *
+     * 
      * @return the documentation.
      */
     public String getDocumentation()
@@ -209,7 +209,7 @@ public class MethodData
 
     /**
      * Tells if this method returns something.
-     *
+     * 
      * @return boolean
      */
     public boolean isReturnTypePresent()
@@ -224,6 +224,9 @@ public class MethodData
     {
         MethodData other = (MethodData)object;
         int result = getMetafacadeName().compareTo(other.getMetafacadeName());
-        return (result != 0) ? result : getName().compareTo(other.getName());
+
+        // Use the characteristic key in order to have a deterministic order
+        return (result != 0) ? result : buildCharacteristicKey().compareTo(
+                other.buildCharacteristicKey());
     }
 }
